08836fc1f56607cb1bb518dc79d41e4c7d1d004d
Ayush saini <ayushsaini7717@gmail.com>
2025-10-17 12:29:42 +0530

    fix/navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -23,7 +23,7 @@ export function Navigation() {
             href="/"
             className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-orange-400"
           >
-            Devbhoomi
+            BTKIT Dwarahat
           </Link>
 
           {/* Desktop Menu */}
